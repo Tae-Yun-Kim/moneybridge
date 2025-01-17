ac268bf55676ac4dc6f516988ec34a9f169cdcbb
lovel1114@naver.com <lovel1114@naver.com>
2025-01-17 16:48:50 +0900

    #	backend/src/main/java/com/moneybridge/domain/member/Member.java #	backend/src/main/java/com/moneybridge/security/CustomUserDetailsService.java #	backend/src/test/java/com/moneybridge/LoanProductServiceTests/LoanProductServiceTests.java #	backend/src/test/java/com/moneybridge/repository/MemberRepositoryTests.java

---
+++ backend/src/main/java/com/moneybridge/domain/loan/LoanProduct.java
@@ -0,0 +1,27 @@
+package com.moneybridge.domain.loan;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "loan_product")
+@Data
+public class LoanProduct {
+
+    @Id
+    private String finPrdtCd; // 금융상품 코드 (PK)
+
+    private String dclsMonth;          // 공시 제출월
+    private String finCoNo;            // 금융회사 코드
+    private String korCoNm;            // 금융회사명
+    private String finPrdtNm;          // 금융상품명
+
+    private String dclsStrtDay;        // 공시 시작일
+    private String dclsEndDay;         // 공시 종료일
+    private String finCoSubmDay;       // 금융회사 제출일
+
+    private String crdtPrdtType;       // 대출종류 코드
+    private String crdtPrdtTypeNm;     // 대출종류명
+    private String crdtLendRateTypeNm; // 금리구분
+    private Double crdtGradAvg;        // 평균 금리
+}
