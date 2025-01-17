9a9bfc3f630af95fb6d8a2e34a85f4f5c7dc2907
32won0 <dnjs2tp@naver.com>
2025-01-17 11:13:49 +0900

    데이터베이스 값받아온거 확인

--- backend/src/main/java/com/moneybridge/repository/LoanProductRepository.java
+++ backend/src/main/java/com/moneybridge/repository/LoanProductRepository.java
@@ -2,7 +2,7 @@ package com.moneybridge.repository;
 
 
 
-import com.back.domain.LoanProduct;
+import com.moneybridge.domain.LoanProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- backend/src/test/java/com/moneybridge/repository/LoanProductServiceTests.java
+++ backend/src/test/java/com/moneybridge/repository/LoanProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.moneybridge.repository;
 
-import com.back.domain.LoanProduct;
+import com.moneybridge.domain.LoanProduct;
+import com.moneybridge.service.LoanProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
