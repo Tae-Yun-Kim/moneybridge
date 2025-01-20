581309e5b70557fc1e2f7b359d4b530f3cbb41e0
lovel1114@naver.com <lovel1114@naver.com>
2025-01-20 18:02:49 +0900

    회원가입, 회원수정 버튼 추가

--- frontend/src/components/member/LoginComponent.js
+++ frontend/src/components/member/LoginComponent.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import useCustomLogin from "../../hooks/useCustomLogin";
 import KakaoLoginComponent from "./KakaoLoginComponent";
-import SignupComponent from "./SignupComponent";
 import { Link } from "react-router-dom";
 
 const initState = {
